44c02d2d01618c001c2261a8e1605a764645be9e
Nimbuscloudus <90365524+Nimbuscloudus@users.noreply.github.com>
2022-02-23 16:58:41 +0800

    updated manifest (base)

--- __manifest__.py
+++ __manifest__.py
@@ -1,26 +1,19 @@
 # -*- coding: utf-8 -*-
+
 {
-    'name': "Help desk",  # Module title
-    'summary': "Help desk",  # Module subtitle phrase
+    'name': 'HelpDesk v0.1',
+    'version': '1.0',
+    'sequence': -100, #//shows up first on the app list the lower it is
+    'category': 'Productivity',
+    'author': 'EDI',
     'description': """
-Manage Library
-==============
-Description related to helpdesk.
-    """,  # Supports reStructuredText(RST) format
-    'author': "Parth Gajjar",
-    'website': "http://www.example.com",
-    'category': 'Library',
-    'version': '14.0.1',
-    'depends': ['base'],
-
+        Client Support """,
+    'depends':[],
     'data': [
-        'security/ir.model.access.csv',
-        'views/help_desk.xml',
+        'security/ir.model.access.csv', 
+        'views/view.xml',
     ],
-    'auto_install': False,
-
-    # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
-    # 'demo': [
-    #     'demo.xml'
-    # ],
+    'installable': True,
+    'application': True, #//declaring this an installable and usable stand-alone app
+    'auto_install' : False,
 }
